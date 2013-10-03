0cf714c999b775c37718d5265a9f869d4f758792
lopezbertoni <lopezbertoni@gmail.com>
2013-10-03 18:39:31 -0400

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace TestRichTextBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddRight = new System.Windows.Forms.Button();
+            this.btnAddLeft = new System.Windows.Forms.Button();
+            this.txtText = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnAddRight
+            // 
+            this.btnAddRight.Location = new System.Drawing.Point(13, 22);
+            this.btnAddRight.Name = "btnAddRight";
+            this.btnAddRight.Size = new System.Drawing.Size(129, 36);
+            this.btnAddRight.TabIndex = 0;
+            this.btnAddRight.Text = "Add Text Right";
+            this.btnAddRight.UseVisualStyleBackColor = true;
+            this.btnAddRight.Click += new System.EventHandler(this.btnAddRight_Click);
+            // 
+            // btnAddLeft
+            // 
+            this.btnAddLeft.Location = new System.Drawing.Point(13, 64);
+            this.btnAddLeft.Name = "btnAddLeft";
+            this.btnAddLeft.Size = new System.Drawing.Size(129, 36);
+            this.btnAddLeft.TabIndex = 1;
+            this.btnAddLeft.Text = "Add Text Left";
+            this.btnAddLeft.UseVisualStyleBackColor = true;
+            this.btnAddLeft.Click += new System.EventHandler(this.btnAddLeft_Click);
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(186, 22);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(313, 186);
+            this.txtText.TabIndex = 2;
+            this.txtText.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 240);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.btnAddLeft);
+            this.Controls.Add(this.btnAddRight);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddRight;
+        private System.Windows.Forms.Button btnAddLeft;
+        private System.Windows.Forms.RichTextBox txtText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestRichTextBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddRight_Click(object sender, EventArgs e)
+        {
+            var txt = String.Format("{0}", "Text Right \r\n");
+            txtText.SelectionAlignment = HorizontalAlignment.Right;
+            txtText.SelectedText = txt;
+        }
+
+        private void btnAddLeft_Click(object sender, EventArgs e)
+        {
+            var txt = "Text Left \r\n";
+            txtText.SelectionAlignment = HorizontalAlignment.Left;
+            txtText.SelectedText = txt;
+        }
+    }
+}
